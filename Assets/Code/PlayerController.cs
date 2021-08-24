ccd5d21f5e3f8defdb68043c07ae9499f365d7e0
Hyperspace146 <adriandinh146@gmail.com>
2021-08-24 15:56:47 -0700

    Started implementing velocity-based ground movement and friction, needs tweaking

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 12f;
-    public float jumpHeight = 5000f;
+    public float GroundAcceleration = 12f;
+    public float MaxGroundedSpeed = 15;
+    public float Friction = 1;
+    public float JumpHeight = 5000f;
 
-    public Transform groundCheck;
-    public float groundDist = 0.4f;
-    public LayerMask groundMask;
+    public Transform GroundCheck;
+    public float GroundDist = 0.4f;
+    public LayerMask GroundMask;
 
     [Tooltip("Change in angle of velocity per second when strafing (in degrees).")]
-    public float deltaTheta = 0.0007f;
+    public float DeltaTheta = 0.0007f;
 
-    public bool isGrounded;
+    private bool isGrounded;
 
     private Rigidbody rb;
     private Collider collider;
@@ -30,41 +32,66 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             print("jump");
-            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y));
+            rb.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y));
         }
     }
 
     void FixedUpdate()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask);
+        isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDist, GroundMask);
 
         if (isGrounded)
+        {
+            //rb.drag
             GroundMove();
+        }
         else
-            AirMove();
+        {
+            // AirMove();
+            AirStrafe();
+        }
     }
 
     void GroundMove()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        // Move the player according to directional input
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection.Normalize();
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        rb.velocity += moveDirection * GroundAcceleration * Time.fixedDeltaTime;
 
-        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+        // Cap grounded speed
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, MaxGroundedSpeed);
+
+        // Apply friction in opposite direction of current velocity
+        // Option 1: rb.AddForce(rb.velocity.normalized * -1 * Friction);
+        // Try rigidbody drag maybe?
+
+        // the *= makes us slow down very slow when we're initially moving very slow (i think). try straight subtracting from velocity instead?
+        // but then we'll slow down very slowly if we're moving really fast. maybe that's what we want though?
+        float speed = rb.velocity.magnitude;
+        float frictionMultiplier = speed - (Friction * Time.fixedDeltaTime);
+        frictionMultiplier = Mathf.Max(0, frictionMultiplier);
+        if (speed > 0) frictionMultiplier /= speed;
+        float fricX = rb.velocity.x * frictionMultiplier;
+        float fricZ = rb.velocity.z * frictionMultiplier;
+        rb.velocity = new Vector3(fricX, rb.velocity.y, fricZ);
+
+        print(rb.velocity);
     }
 
-    void AirMove()
+    void AirStrafe()
     {
         float x = Input.GetAxis("Horizontal");
-        float theta = deltaTheta * Time.fixedDeltaTime;
+        float theta = DeltaTheta * Time.fixedDeltaTime;
         if (x > 0)
         {
-            theta = deltaTheta * -1;
+            theta = DeltaTheta * -1;
         }
         else if (x < 0)
         {
-            theta = deltaTheta;
+            theta = DeltaTheta;
         }
         else
         {
@@ -74,6 +101,8 @@ public class PlayerController : MonoBehaviour
 
         Vector3 newVelocity = new Vector3(0, rb.velocity.y, 0);
 
+        // Use matrix multiplication using the rotation matrix to rotate our current velocity vector 
+        // by theta degrees (the magnitude of our velocity vector still stays the same)
         newVelocity.x = Mathf.Cos(theta) * rb.velocity.x + -Mathf.Sin(theta) * rb.velocity.z;
         newVelocity.z = Mathf.Sin(theta) * rb.velocity.x + Mathf.Cos(theta) * rb.velocity.z;
 
