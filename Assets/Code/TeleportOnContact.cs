2fb78b59ceaebff5e09cc7b9792d22c8085137d9
Hyperspace146 <adriandinh146@gmail.com>
2021-08-27 21:04:25 -0700

    Started developing first test level

---
+++ Assets/Code/TeleportOnContact.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportOnContact : MonoBehaviour
+{
+    public Transform TeleportDestination;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.transform.position = TeleportDestination.position;
+        collision.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+}
