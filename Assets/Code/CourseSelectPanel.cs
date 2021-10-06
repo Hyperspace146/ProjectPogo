60fd236ae93c1e75293df92e332db2a91028f7e9
Hyperspace146 <adriandinh146@gmail.com>
2021-10-05 17:12:49 -0700

    implemented course select menu for the test level

---
+++ Assets/Code/CourseSelectPanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CourseSelectPanel : MonoBehaviour
+{
+    public GameObject Player;
+    public Transform PogoCourseStart;
+    public Transform GeneralCourseStart;
+
+    public void TeleportToPogoCourse()
+    {
+        Player.transform.position = PogoCourseStart.position;
+    }
+
+    public void TeleportToGeneralCourse()
+    {
+        Player.transform.position = GeneralCourseStart.position;
+    }
+}
